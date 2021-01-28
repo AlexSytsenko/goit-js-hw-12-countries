dc0f8aa26bc7aee2a697f8e69e77d6474f44a3e9
AlexSytsenko <sytsenko@gmail.com>
2021-01-28 14:08:39 +0200

    add start files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './styles.css';
+
+
+
+// fetch('https://jsonplaceholder.typicode.com/users')
+//   .then(response => {
+//     console.log(response);//response handling
+//   })
+//   .then(data => {
+//     console.log(data);// data handling
+//   })
+//   .catch(error => {
+//     console.log(error);// error handling
+//   });
